f8b905d65717373e16b47ac801c9e472ba81080d
berluche prunier <bpmac@MacBook-Air-2.local>
2023-11-13 23:10:43 -0500

    button removed

--- src/components/MovieBanner.jsx
+++ src/components/MovieBanner.jsx
@@ -29,9 +29,6 @@ const truncateString = (str, num) => {
           <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
           <div className='absolute w-full top-[20%] p-4 md:p-8'>
                 <h1 className='text-3xl md:text-5xl'>{movie?.title}</h1>
-            <div className='my-4'>
-                <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Learn-More</button>
-            </div>
             <p className='text-gray-800 text-sm '>Released: {movie?.release_date}</p>
             <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-black font-semibold'>{truncateString(movie?.overview, 200)}</p>
             </div>
